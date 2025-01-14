a97e1a4fa512548491569777abb490b7d5c8f30a
Sameer Reddy <sameredd@cisco.com>
2025-01-14 16:41:37 -0500

    Update mini math model and add larger math processing script

--- config/mini_math.py
+++ config/mini_math.py
@@ -8,7 +8,7 @@ init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = 'nanogpt'
-wandb_run_name = 'minigpt-math-v2-embed-more-norm-no-factorize'
+wandb_run_name = 'minigpt-math-v2-embed-more-norm-no-factorize-bigger'
 out_dir = os.path.join('out', wandb_run_name)
 # data
 dataset = 'math' # 'openwebtext' or 'shakespeare_char' or 'math
@@ -18,7 +18,7 @@ batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch siz
 # model # NOTE MODERN BERT USES DEEP AND NARROW LAYERS! 22 layers, 768 hidden size, 150 M params
 # this one h
 block_size = 128 # sequence length
-n_layer = 8
+n_layer = 12
 n_embd = 384
 n_head = 4
 norm_gain = False
